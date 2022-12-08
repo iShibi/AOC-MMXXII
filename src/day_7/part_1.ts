6501725becc23352965515715b40138f0b111d13
Shubham Parihar <shubhamparihar391@gmail.com>
2022-12-08 10:02:36 +0530

    feat: add solution for day 7 part 2

--- src/day_7/part_1.ts
+++ src/day_7/part_1.ts
@@ -1,32 +1,65 @@
 const inputFilePath = `${Deno.cwd()}/src/day_7/input.txt`;
 const inputText = await Deno.readTextFile(inputFilePath);
-const lines = inputText.split('\n').slice(0, -1).reverse();
+const lines = inputText.split('\n').slice(0, -1);
 
+const childToParent = new Map<string, string>();
+const parentToChildren = new Map<string, Array<string>>();
 const dirSizes = new Map<string, number>();
+let currentDir = '';
 let currentDirSize = 0;
-let total = 0;
 
 for (const line of lines) {
 	const words = line.split(' ');
 	if (words.at(0) === '$') {
 		const [_, cmd, arg] = words as ['$', 'cd' | 'ls', string | undefined];
-		if (cmd === 'cd') {
-			if (arg === '..') continue;
-			dirSizes.set(arg as string, currentDirSize);
-			if (currentDirSize <= 100000) {
-				total += currentDirSize;
+		if (cmd === 'cd' && typeof arg !== 'undefined') {
+			if (arg === '..') {
+				const parent = childToParent.get(currentDir);
+				if (parent) {
+					currentDir = parent;
+				}
+			} else if (arg === '/') {
+				currentDir = '/';
+			} else {
+				const moveInto = `${currentDir}>${arg}`;
+				childToParent.set(moveInto, currentDir);
+				currentDir = moveInto;
 			}
 			currentDirSize = 0;
 		}
 	} else if (words.at(0) === 'dir') {
-		const dirName = words.at(1);
-		if (!dirName) continue;
-		const dirSize = dirSizes.get(dirName);
-		if (!dirSize) continue;
-		currentDirSize += dirSize;
+		const [_, dirName] = words as ['dir', string];
+		if (parentToChildren.has(currentDir)) {
+			parentToChildren.get(currentDir)?.push(`${currentDir}>${dirName}`);
+		} else {
+			parentToChildren.set(currentDir, [`${currentDir}>${dirName}`]);
+		}
 	} else {
 		const [fileSize, _] = words.map((word) => Number.parseInt(word));
 		currentDirSize += fileSize;
+		dirSizes.set(currentDir, currentDirSize);
+	}
+}
+
+function calculateTotalSizeOfDir(dirName: string) {
+	let totalDirSize = dirSizes.get(dirName) ?? 0;
+	const children = parentToChildren.get(dirName);
+	if (children?.length) {
+		for (const child of children) {
+			totalDirSize += calculateTotalSizeOfDir(child);
+		}
+	}
+	dirSizes.set(dirName, totalDirSize);
+	return totalDirSize;
+}
+
+calculateTotalSizeOfDir('/');
+
+let total = 0;
+
+for (const dirSize of dirSizes.values()) {
+	if (dirSize <= 100000) {
+		total += dirSize;
 	}
 }
 
